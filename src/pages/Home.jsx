f535ab54a0478fe05c2ed81a4edd103dff8ba498
ayushlohmod <ayushlohmod@gmail.comd>
2023-08-08 16:01:12 +0530

    added new files and do work on cusyomization

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -53,7 +53,6 @@ const Home = () => {
                         title="Customize It"
                         handleClick={() => state.intro = false}
                         customStyles='w-fit px-4 py-2.5 font-bold text-sm'
-                    
                     />
                 </motion.div>
                 </motion.div>
